048526f4d3cf6a710193dbe67228e9279a3a9f17
kimuit117 <kemoikiuita@gmail.com>
2022-08-26 15:44:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Code your solutions in this file
 
 let names = ["Orengo", "Ole sapit", "Ledama"];
+
 function writeCards(names, eventName) {
   let thankCard = [];
+
   for (let i = 0; i < names.length; i++) {
+    
     thankCard.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
   }
   return thankCard;
@@ -14,6 +17,7 @@ console.log(writeCards(names, "birthday"));
 
 function countDown() {
     let count = 0;
+   
     while (count < 11) {
         console.log (count++);
     }
